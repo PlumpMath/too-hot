cac0e80e1c366dca73a51367406e3f488a532961
Lauri Oherd <lauri.oherd@gmail.com>
2016-03-27 06:56:00 +0300

    Fix project url.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.clojars.lauri/too-hot "1.0.0"
   :description "A very simple Fahrenheit to Celsius converter."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/laurio/too-hot"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
